9596a98e6c642f5989a2d5a84d9ae3c790b7e5f6
Steven <steven.codestack@gmail.com>
2025-07-06 19:18:29 +0800

    HomeView Done

---
+++ src/components/Navbar/LinksData.js
@@ -0,0 +1,29 @@
+export const desktopNav = [
+	{ title: 'Pendidikan', link: '/' },
+	{
+		title: 'Tentang Kami',
+		link: '/',
+		children: [
+			{ title: 'Fasilitas', link: '/' },
+			{ title: 'Struktur Organisasi', link: '/' }
+		]
+	},
+	{ title: 'Kehidupan Kampus', link: '/' },
+	{ title: 'Riset & Pengabdian', link: '/' }
+]
+
+export const mobileNav = [
+	{ title: 'Beranda', link: '/' },
+	{ title: 'Pendidikan', link: '/' },
+	{ title: 'Tentang Kami', link: '/' },
+	{ title: 'Kehidupan Kampus', link: '/' },
+	{ title: 'Riset & Pengabdian', link: '/' },
+	{ title: 'Penerimaan', link: '/' }
+]
+
+export const addNav = [
+	{ title: 'Mahasiswa', link: '/' },
+	{ title: 'Fasilitas', link: '/' },
+	{ title: 'Berita & Kegiatan', link: '/' },
+	{ title: 'Kontak', link: '/' }
+]
